ae9d0024cb97adac57de5c184d3addb06483597d
Zhao Xiaohan <2981531232@qq.com>
2023-10-23 21:32:02 +0800

    表单的简单验证

--- demo-0/src/components/Login.vue
+++ demo-0/src/components/Login.vue
@@ -1,15 +1,13 @@
 <script setup>
 import { Avatar, Lock } from '@element-plus/icons-vue'
+import {ref} from 'vue'
 </script>
 
 <template>
     <div class="loginPage">
         <div class="loginBox">
-            <!-- <div style="flex: 1;">
-                <img src='..\assets\start_page.jpg' style="width: 100%;height: 100%;">
-            </div> -->
             <div style="flex: 1; display: flex; align-items: center; justify-content: center;">
-                <el-form :model="user" style="width: 80%;">
+                <el-form :model="user" style="width: 80%;" :rules="rules" ref="loginRef">
                     <div style="font-size: 20px; font-weight: bold; text-align: center;  margin-bottom: 20px;">
                         欢迎登录
                     </div>
@@ -20,7 +18,7 @@ import { Avatar, Lock } from '@element-plus/icons-vue'
                         <el-input prefix-icon="Lock" size="medium" show-password v-model="user.password" placeholder="请输入密码"></el-input>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary" style="width: 100%;">
+                        <el-button type="primary" style="width: 100%;" @click="EventLogin">
                             登录
                         </el-button>
                     </el-form-item>
@@ -41,17 +39,27 @@ export default {
             user: {
                 username: '',
                 password: ''
+            },
+            rules:{
+                username:[
+                    {required : true, message : "请输入账号", trigger : 'blur'}
+                ],
+                password:[
+                    {required : true, message : "请输入密码", trigger : 'blur'}
+                ]
             }
         }
     },
-    // setup(){
-    //     return{
-    //         User, Lock
-    //     }
-    // },
-
-    methods() {
-
+    methods: {
+        EventLogin(){
+            this.$refs.loginRef.validate((valid)=>{         // 表单自身rules的规则检查
+                if(valid){
+                    // 这里与后端交互
+                    //console.log("请求成功")
+                    this.$router.push('/Map')
+                }
+            })
+        }
     }
 
 
